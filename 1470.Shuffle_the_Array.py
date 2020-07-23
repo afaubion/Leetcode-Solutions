294f1dbf7ff966da15750ae087fe1cb682afe51d
Aaron <agfaubion@gmail.com>
2020-07-23 12:10:09 -0500

    Delete 1470.Shuffle_the_Array.py

--- 1470.Shuffle_the_Array.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def shuffle(self, nums, n):
-        """
-        :type nums: List[int]
-        :type n: int
-        :rtype: List[int]
-        """
-        return [i for sub in zip(nums[:n], nums[n:]) for i in sub]
